b5764fe9866deab59df72b7d6eed98d1959bf1f8
MokhtarAkle <mokhtar.akle@hva.nl>
2023-05-29 18:51:49 +0200

    added express session middleware and socket io back end

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var app = express();
 var http = require('http');
 var port = process.env.PORT || 3000;
 var server = http.createServer(app);
+const socketio = require("socket.io");
+const session = require("express-session");
+const io = socketio(server);
+
 require('dotenv').config()
 
 
@@ -20,6 +24,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+
+const sessionMiddleware = session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        //set expiry time for session to 7 days
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+});
+
+//using the session middleware
+app.use(sessionMiddleware);
+io.engine.use(sessionMiddleware);
+
+//listening for user connection
+io.on("connection", (socket) => {
+    console.log("a user connected");
+//More Code To Be Added Here
+});
 
 server.listen(port, () => {
     console.log('listening on http://localhost:' + port);
